a8638edf3ee1203867ed76ae088ef0b8ef4889be
Augusto Medeiros <augusto.med@gmail.com>
2018-07-12 22:25:32 -0700

    Add QDarkStyleSheet as submodule and use it

--- pubfix/main.cpp
+++ pubfix/main.cpp
@@ -1,9 +1,25 @@
 #include "MainWindow.h"
+
 #include <QApplication>
+#include <QFile>
+#include <QTextStream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile f(":qdarkstyle/style.qss");
+    if (!f.exists())
+    {
+        printf("Unable to set stylesheet, file not found\n");
+    }
+    else
+    {
+        f.open(QFile::ReadOnly | QFile::Text);
+        QTextStream ts(&f);
+        a.setStyleSheet(ts.readAll());
+    }
+
     MainWindow w;
     w.show();
 
