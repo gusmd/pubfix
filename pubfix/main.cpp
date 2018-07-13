b3429477398474b29f0ba12441900ff83b1fe32e
Augusto Medeiros <augusto.med@gmail.com>
2018-07-12 22:55:58 -0700

    Clean up lazy main file

--- pubfix/main.cpp
+++ pubfix/main.cpp
@@ -8,17 +8,11 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    // Load stylesheet from resource and set it
     QFile f(":qdarkstyle/style.qss");
-    if (!f.exists())
-    {
-        printf("Unable to set stylesheet, file not found\n");
-    }
-    else
-    {
-        f.open(QFile::ReadOnly | QFile::Text);
-        QTextStream ts(&f);
-        a.setStyleSheet(ts.readAll());
-    }
+    f.open(QFile::ReadOnly | QFile::Text);
+    QTextStream ts(&f);
+    a.setStyleSheet(ts.readAll());
 
     MainWindow w;
     w.show();
